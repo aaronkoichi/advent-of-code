c4341b15d77704853683e57c449e760c07d06bd7
zlee <zlee@u81z05s03.42kl.edu.my>
2024-12-10 19:16:03 +0800

    day 2 gold

--- day2/day2_part2.py
+++ day2/day2_part2.py
@@ -2,9 +2,8 @@ file = open("input.txt", "r")
 lines = file.readlines()
 
 safe_counter = 0
-for x in lines:
-    list1 = x.split(" ")
-    list1 = list(map(int, list1))
+
+def ft_safe_checker(list1):
 
     print(list1);
     boolean = False
@@ -26,7 +25,7 @@ for x in lines:
 
     if boolean == False:
         print("end")
-        continue
+        return False;
     else:
         for count in range(len(list1) - 1):
             difference = abs(list1[count] - list1[count + 1])
@@ -37,7 +36,29 @@ for x in lines:
                 break
 
         if boolean == True:
-            safe_counter += 1
             print("yes")
+            return True
+        else:
+            return False
 
+for x in lines:
+    list1 = x.split(" ")
+    list1 = list(map(int, list1))
+    checker = ft_safe_checker(list1)
+    if not checker:
+        for index in range(len(list1)): 
+            temp = list()
+            for i in range(len(list1)):
+                if (i == index):
+                    continue
+                else:
+                    temp.append(list1[i])
+            checker = ft_safe_checker(temp)
+            if not checker:
+                continue
+            else:
+                safe_counter += 1
+                break
+    else:
+        safe_counter += 1
 print(safe_counter)
